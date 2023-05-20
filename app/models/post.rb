407d18ed08be12ea513356dd2f9fdb0f26ef2a87
Frempong <samankwaa.frempong@gmail.com>
2023-05-20 04:56:09 +0000

    Add validation for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true, length: {maximum: 250}
+  validates :comments_counter, numericality: {only_intger: true, greater_than_or_equal_to: 0}
+  validates :like_counter, numericality: {only_intger: true, greater_than_or_equal_to: 0}
+
   after_save :update_user_post_counter
 
   def five_recent_comments
