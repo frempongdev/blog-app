603dfa320eb5e314b4bba02b4d9a54fab4c8a22a
Frempong <samankwaa.frempong@gmail.com>
2023-05-20 04:47:10 +0000

    Add validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :like, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: {only_intger: true, greater_than: 0}
+
   def last_three_posts
     posts.order(created_at: :desc).limit(3)
   end
