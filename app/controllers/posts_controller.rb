fedf1fd8108f4565fa0ce8938551bf5ce365859a
Frempong <samankwaa.frempong@gmail.com>
2023-05-27 07:36:49 +0000

    Updated user and post controllers with variables for reference

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,39 @@
 class PostsController < ApplicationController
-  def index; end
+  def index 
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def new
+    @post = @current_user.posts.new
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.like_counter = 0
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_posts_path(@user)
+        else
+          render :new, locals: { post: @post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+    
 end
