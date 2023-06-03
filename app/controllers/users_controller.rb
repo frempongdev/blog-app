20a0c02ffc385d948d59e54647c417f15a669a89
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-03 11:07:56 +0000

    complete sign out path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.all
   end
 
- def show
+  def show
     if params[:id] == 'sign_out'
       sign_out_and_redirect
     else
       @user = User.find(params[:id])
     end
   end
+
+  private
+
+  def sign_out_and_redirect
+    sign_out current_user
+    redirect_to new_user_session_path
+  end
+
 end
-
-
