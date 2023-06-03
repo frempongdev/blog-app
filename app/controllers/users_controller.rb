542566785870b2601d5aee30c300ed4db3637ad2
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-03 11:03:58 +0000

    Update the show action in user controllers to enable signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id] == 'sign_out'
+      sign_out_and_redirect
+    else
+      @user = User.find(params[:id])
+    end
   end
 end
